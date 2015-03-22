5ff666472f2b48a1ea86636b010998b0ba5963f2
markiewb <benno.markiewicz@googlemail.com>
2015-03-22 17:03:30 +0100

    update license in headers

--- nb-pasteasfile-java/src/main/java/de/markiewb/plugins/netbeans/pasteasfile/handler/java/JavaHandler.java
+++ nb-pasteasfile-java/src/main/java/de/markiewb/plugins/netbeans/pasteasfile/handler/java/JavaHandler.java
@@ -1,5 +1,5 @@
-/*
- * Copyright 2015 Benno Markiewicz (benno.markiewicz@googlemail.com).
+/* 
+ * Copyright 2015 markiewb.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
